e113a6e344c2b4d4744ab473b8a95d7c1a15936d
sunita baram <sunitabaram489@gmail.com>
2025-01-31 16:21:46 +0545

    comments and arithmetic operators added

---
+++ script.js
@@ -0,0 +1,34 @@
+//Comments in JavaScript
+//this prints hello world
+/*this prints hello
+world in the console*/
+// console.log("hello world!")
+
+//Arthimetic Operators
+// let a = 5;
+// let b = 2;
+// console.log("a = ",a , "& b = ",b);
+// console.log("a + b = ", a + b);
+// console.log("a - b = ",a - b);
+// console.log("a * b = ",a * b);
+// console.log("a / b = ",a/b);
+// console.log("a % b = ",a % b);
+// console.log("a ** b = ",a**b);
+
+//Unary Operators
+let a = 5;
+let b = 2;
+// a--;
+console.log("a = ",a , "& b = ",b);
+// console.log("a = ", a);
+
+// pre increment and decrement => ++a and --a
+// console.log("++a = ",++a);
+// console.log("++a = ", a);
+
+// console.log("--a = ",--a)
+
+// post increment and decrement => a++ and a-- 
+// post increment (a++) => first prints the value and then changes the value in next line.
+// console.log("a++ = ",a++); //prints 5
+// console.log("a++ ", a); //and then chagnes the value to 6 in next line
