22ee239a88e5fa2b1dddc9b62e2c91ddad9f70f5
sunita baram <sunitabaram489@gmail.com>
2025-02-04 15:20:32 +0545

    Added For loop, while loop, do-while loop, strings and added program.py

--- script.js
+++ script.js
@@ -174,6 +174,27 @@ world in the console*/
 //         console.log(`Sorry, we are out of ${exp}`);
 // }
 
+// let a = Number(prompt("Enter first number: "));
+// let b = Number(prompt("Enter second number: "));
+// let exp = prompt("Enter your expression (+, -, *, /): ");
+// switch (exp){
+//     case '-':
+//         console.log(a - b);
+//         break;
+//     case '*':
+//         console.log(a * b);
+//         break;
+//     case '/':
+//         console.log(a / b);
+//         break;
+//     case '+':
+//         console.log(a + b);
+//         break;
+//     default:
+//         console.log("Invalid operator. Please enter '+', '-', '*', '/'");
+// }
+
+
 //Let's Pratice
 //QN. 1 Get user to input a number using prompt ("Enter a number:"). Check if the number is a multiple of 5 or not.
 // let num = prompt("Enter a number: ");
@@ -204,3 +225,164 @@ world in the console*/
 // console.log(grade);
 
 //Loops & Strings
+// for (let i = 1; i<=5; i++){
+//     console.log("Apna College");
+// }
+
+//Calculate sum of 1 to 5
+// let sum = 0;
+// for (let i = 1; i <=5; i++){
+//     sum += i;
+// }
+// console.log(sum);
+
+//Calculate sum of 1 to n
+// let sum = 0;
+// let n = Number(prompt("Enter a number: "));
+// for (let i = 1; i<=n; i++){
+//     sum += i;
+// }
+// console.log("sum = ",sum);
+
+//Infinite Loop
+//loop that never ends
+
+//while loop
+// let i = 1;
+// while (i<=5){
+//     console.log("i = ",i);
+//     i++;
+// }
+
+//do-while loop -> even though the condition is false it runs once.
+// let i = 11;
+// do{
+//     console.log("i = ",i);
+// }while(i<=10);
+
+//for-of loop -> helps to iterate in string and arrays
+// let str = "ApnaTimeAyega";
+// let len = 0;
+// for (let i of str){
+//     console.log("i = ",i);
+//     len++;
+// }
+// console.log("lenght of string is ",len);
+
+//for-in loop => works in objects and arrays only
+// let student = {
+//     name : "Sunita Baram",
+//     age : 24,
+//     cgpa : 7.5,
+//     isPass : true,
+//     country : "America"
+// };
+
+// for (let key in student){
+//     console.log("key = ",key, "val = ", student[key]);
+// }
+
+//Let's pratice
+//QN. 1 Print all even numbers from 0 to 100.
+// for (let i = 0; i <= 100; i++){
+//     if(i % 2 == 0){
+//         console.log(i);
+//     }
+// }
+
+//QN 2. Create a game where you start with any random game number. Ask the user to keep guessing the game number until the user enters correct value.
+// let a =174
+// let b ="9"
+// console.log(a + b);
+
+// let gameNumber = 36;
+// let userNumber = Number(prompt("Guess the game number:"));
+// while (userNumber !== gameNumber){
+//     if (userNumber > gameNumber){
+//         userNumber = Number(prompt("Your guess number is greater than game number. Try lesser number"));
+//     }else{
+//         userNumber = Number(prompt("Your guess is lesser than game number. Try greater number"));
+//     }
+
+// }
+// console.log("Congratulations! You have guessed the correct number",gameNumber);
+
+//WAP to check if a number given by user is prime or not
+// num = Number(prompt("Enter a number: "));
+// count = 0;
+// for (let i = 1; i <= num; i++){
+//     if (num % i === 0){
+//         count++;
+//     }
+// }
+// if (count === 2){
+//     console.log("Prime Number");
+// }else{
+//     console.log("Not Prime");
+// }
+
+//Strings: sequence of characters 
+// let str = "ApnaCollege";
+// console.log(str[9]);
+
+//Template Literals
+// let specialString = `This is a template literal`;
+// console.log(specialString);
+
+// let obj = {
+//     item : 'pen',
+//     price : 10
+// };
+// ``-> back tick
+// console.log("The cost of", obj.item, "is", obj.price, "rupees");
+//printing by using template literals
+// console.log(`The cost of ${obj.item} is ${obj.price} rupees`);
+
+//Escape characters [\n, \t]
+// let str = "Apna\nCollege"; //prints college in next line
+// console.log(str.length); //prints 12 becoz it takes escape characters as a single char
+// let str = "Apna\tCollege";
+// console.log(str.length);
+
+//String Methods in JS
+//str.toUpperCase()
+// let str = "ApnaCollege";
+// str = str.toUpperCase();
+// console.log(str);
+
+// let str = "ApnaCollege";
+// str = str.toLowerCase();
+// console.log(str);
+
+// let str = "     Apna College  JS       ";
+// console.log(str.trim()); //removes whitespaces from before and after string
+
+//str.slice(start, end) => returns part of string
+// let str = "ApnaCollege";
+// console.log(str.slice(1,-1));
+
+//str1.concat(str2) //joins str2 with str1
+// let str1 = "apna";
+// let str2 = "college";
+// let res = str1.concat(str2);
+// console.log(res);
+//OR
+// console.log(str1 + str2);
+// console.log("hello"+123); //JS implicitly converts number to string
+
+//str.replace(searcVal, newVal)
+// let str = "hello";
+// console.log(str.replace('lo','p'));
+
+//str.charAt(index)
+// let str = "hello";
+// console.log(str.charAt(4));
+
+//QN1. Propmt the user to enter their full name. Generate a username for them based on the imput. Start username with @, followed by thier full name and ending with the fullname length.
+// userName = prompt("Enter your full name: ");
+// // userName_len = userName.length;
+// // console.log(`Your username name is @${userName}${userName_len}`);
+
+// //or 
+// username = '@'+userName+userName.length;
+// console.log(username);
